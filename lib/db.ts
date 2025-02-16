6925dcedce530b97b1e8915cf0311557fda4f4b2
aidanmcintosh07 <aidanmcintosh@gmail.com>
2025-02-16 18:16:02 -0400

    changed db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,16 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { PrismaClient } from "../../prisma/generated/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+let prisma: PrismaClient;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+	prisma = new PrismaClient();
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
+	prisma = global.prisma;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
